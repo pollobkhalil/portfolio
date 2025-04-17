49d62586e03dc606880f61f814a5d70475ba091a
Your Name <you@example.com>
2025-04-17 12:07:49 +0600

    add text slide animation

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,13 @@
+import Hero from "./sections/Hero"
+
 const App = () => {
   return (
-    <main>
+    <>
 
-        <div>
-        <h1>Welcome to my Portfolio</h1>
-        </div>
-      
-    </main>
+    
+    
+    <Hero />
+    </>
   )
 }
 
---
+++ src/constants/index.js
@@ -0,0 +1,7 @@
+
+export const words = [
+    {text: 'Ideas', imagPath: '/images/ideas.svg'},
+    {text: 'Concepts', imagPath: '/images/concepts.svg'},
+    {text: 'Designs', imagPath: '/images/designs.svg'},
+    {text: 'Code', imagPath: '/images/code.svg'},
+  ]
